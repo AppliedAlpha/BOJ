29e899c2d003ad09f20f03fb2e00ce65ebe69724
Aria <applied7076@naver.com>
2020-02-11 19:21:33 +0900

    ~2020.02.11

---
+++ Submitted/11xxx/11689.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+// euler-pi problem
+// n = p1^k1 x p2^k2 ... pm^km
+// pi(n) = n x (1-1/p1) x (1-1/p2) ... x (1-1/pm)
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n;
+    ll res = 1, prime = 2;
+    while (prime * prime <= n) {
+        ll cnt = 0;
+        while (!(n % prime)) {
+            n /= prime;
+            res *= prime;
+            cnt++;
+        }
+        if (!cnt) {
+            prime++;
+            continue;
+        }
+        res = res * (prime - 1) / prime;
+        prime++;
+    }
+    if (n != 1) res *= n-1;
+    cout << res;
+    return 0;
+}
